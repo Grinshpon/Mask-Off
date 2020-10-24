968acd085119b2fd3b51abfda73530d2d381d5aa
Grinshpon <grinshpondaniel@protonmail.com>
2020-10-24 18:38:21 -0400

    work on menus

---
+++ Assets/Scripts/Menu/Resolutions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Resolutions : MonoBehaviour
+{
+  Resolution[] resolutions;
+  public Dropdown dropdownMenu;
+
+  void Start()
+  {
+    resolutions = Screen.resolutions;
+    dropdownMenu.onValueChanged.AddListener(delegate { Screen.SetResolution(resolutions[dropdownMenu.value].width, resolutions[dropdownMenu.value].height, false); });
+    for (int i = 0; i < resolutions.Length; i++)
+    {
+      string text = ResToString(resolutions[i]);
+      dropdownMenu.options.Add(new Dropdown.OptionData(text));
+      dropdownMenu.value = i;
+    }
+  }
+
+  string ResToString(Resolution res)
+  {
+    return res.width + " x " + res.height;
+  }
+}
